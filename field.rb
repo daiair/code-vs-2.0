c32834d099c3325f4e5ef6736c20974a37035613
齋藤 大之 <dai@Miu.local>
2012-12-06 11:09:18 +0900

    Make Field Class.

---
+++ field.rb
@@ -0,0 +1,14 @@
+#CODE VS 2.0
+#Field Class
+
+class Field
+  def initialize(w, h)
+    @width = w
+    @height = h
+    @field = Array.new(w,Array.new(h,0))
+  end
+
+  def add_block(block, x)
+    block.each do |line|
+
+end
